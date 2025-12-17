c238d211776e9597aec14ae8e596f2a231f1f4e9
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2025-12-17 22:22:49 +0700

    chuyển bị deloy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5001/api/:path*', 
-      },
-    ];
-  },
 };
 
 export default nextConfig;
